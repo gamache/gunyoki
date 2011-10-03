f57db367c9e1d7ecb4f295ff41544c132b369a48
pete gamache <gamache@gmail.com>
2011-10-03 11:59:04 -0400

    empty rails 3.1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gunyoki::Application.config.secret_token = '386f4aefaebac5d62b8f1454e73a5aecfa81bbca8bbdfa1447f6394727dd5100af247f492e87604f7fa8c7dcc2dd849ae5b7f43eff55d779aadc1856f7f34890'

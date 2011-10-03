2defa145c015cfab20b20200fedcc59b0df28fe3
pete gamache <gamache@gmail.com>
2011-10-03 12:13:06 -0400

    empty models and ctrlrs for game, player, stats

--- Gemfile
+++ Gemfile
@@ -31,3 +31,8 @@ group :test do
   # Pretty printed test output
   gem 'turn', :require => false
 end
+
+group :development do
+  gem 'execjs'
+  gem 'therubyracer'
+end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module Gunyoki
   class Application < Rails::Application
+
+    config.middleware.use Rack::ContentLength
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
